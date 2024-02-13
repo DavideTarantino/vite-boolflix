9577edf2353821b83c5bc45abb2c765b9d615e4a
DavideTarantino <davidetarantino2005@gmail.com>
2024-02-13 15:09:34 +0100

    navbar setup

--- format-vue/src/App.vue
+++ format-vue/src/App.vue
@@ -1,11 +1,27 @@
 <script>
+    import SearchBar from './Components/SearchBar.vue';
 
+    export default {
+        components: {
+            SearchBar
+        },
+        data(){
+            return{}
+        }
+    }
 </script>
 
 <template>
-
+    <header>
+        <SearchBar/>
+    </header>
 </template>
 
 <style lang="scss">
-    @use './Styles/main.scss'
+    @use './Styles/main.scss';
+
+    header{
+        background-color: black;
+        padding-block: 1%;
+    }
 </style>
---
+++ format-vue/src/Components/SearchBar.vue
@@ -0,0 +1,82 @@
+<script>
+    export default {
+        name: 'SearchBar',
+        data(){
+            return{}
+        }
+    }  
+</script>
+
+<template>
+    <div class="flex-container-main">
+        <div class="flex-container-section">
+            <div>
+                <h1>BOOLFLIX</h1>
+            </div>
+            <div id="links">
+                <a href="#">Home</a>
+                <a href="#">Serie TV</a>
+                <a href="#">Film</a>
+                <a href="#">Nuovi e popolari</a>
+                <a href="#">La mia lista</a>
+                <a href="#">Sfoglia per lingua</a>
+            </div>
+        </div>
+        <div class="flex-container-section">
+            <div id="ricerca">
+                <input type="text" name="" id="" placeholder="Cosa desideri vedere ?">
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            <a href="#"><i class="fa-solid fa-bell"></i></a>
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../Styles/partials/mixins' as*;
+
+    .flex-container-main{
+        @include page-layout;
+        display: flex;
+        justify-content: space-between;
+
+        .flex-container-section{
+            display: flex;
+            gap: 15px;
+            align-items: center;
+
+            #links{
+                display: flex;
+                gap: 15px;
+            }
+
+            h1{
+                color: red;
+                cursor: pointer;
+            }
+
+            a{
+                text-decoration: none;
+                color: white;
+            }
+
+            a:hover{
+                color: gray;
+            }
+
+            #ricerca{
+                display: flex;
+                margin-right: 20px;
+
+                input{
+                    padding: 3%;
+                }
+
+                button{
+                    padding: 3%;
+                }
+            }
+        }
+    }
+</style>
